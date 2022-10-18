bb4c668a277260befbca5f483ab99b07de87e6de
adapprogram <adapprogram2022@gmail.com>
2022-10-17 21:29:47 -0500

    backend-add-question-kinestico

--- api/questionAPI.py
+++ api/questionAPI.py
@@ -8,7 +8,7 @@ user_Ref = db.collection('user')
 questionAPI = Blueprint('questionAPI', __name__)
 
 
-@questionAPI.route('/variables')
+@questionAPI.route('/ ')
 def variables():
     try:
         question = [
@@ -623,5 +623,213 @@ def funcionesiterativas4():
             'validation': validation,
             'answer': answer
         }), 200
+    except Exception as e:
+        return f"an error ocurred : {e}"
+
+@ questionAPI.route('/kinestico/estructurasdecision')
+def kinesticoestructurasdecision():
+    try:
+        question = [
+            {
+                "question": "Escribir un programa que pregunte al usuario su edad y muestre por pantalla si es mayor de 12 años.",
+            },
+            {"question": "Escribir un programa que almacene la cadena de caracteres contraseña en una variable, pregunte al usuario por la contraseña e imprima por pantalla si la contraseña introducida por el usuario coincide con la guardada en la variable sin tener en cuenta mayúsculas y minúsculas.",
+
+             },
+            {
+                "question": "Los tramos impositivos para la declaración de la renta en un determinado país son los siguientes:/nRenta	Tipo impositivo/nMenos de 10000€	5%/nEntre 10000€ y 20000€	15%/nEntre 20000€ y 35000€	20%/nEntre 35000€ y 60000€	30%/nMás de 60000€	45%/nEscribir un programa que pregunte al usuario su renta anual y muestre por pantalla el tipo impositivo que le corresponde.",
+    
+            },
+            {
+                "question": "Escribir un programa que pida al usuario un número entero y muestre por pantalla si es par o impar.",
+            }
+        ]
+        return jsonify(question), 200
+    except Exception as e:
+        return f"an error ocurred : {e}"
+
+@ questionAPI.route('/kinestico/estructurasdecision/1', methods=['POST'])
+def kinesticoestructurasdecision1():
+    try:
+        answer = "Es mayor de 12 años"
+        validation = False
+        if request.json['respuesta'].replace("  ","") == answer:
+            validation = True
+
+        return jsonify({
+            'validation': validation,
+            'answer': answer
+        }), 200
+    except Exception as e:
+        return f"an error ocurred : {e}"
+
+@ questionAPI.route('/kinestico/estructurasdecision/2', methods=['POST'])
+def kinesticoestructurasdecision2():
+    try:
+        answer1 = "La contraseña es incorrecta"
+        answer2 = "La contraseña es correcta"
+        answer= ''
+        validation = False
+        if request.json['respuesta'].replace("  ","") == answer1 or request.json['respuesta'].replace("  ","") == answer2:
+            validation = True
+
+        if request.json['respuesta'].replace("  ","") == answer1:
+            answer = answer1
+        else:
+            answer = answer2                    
+        return jsonify({
+            'validation': validation,
+            'answer': answer
+        }), 200
+    except Exception as e:
+        return f"an error ocurred : {e}"
+@ questionAPI.route('/kinestico/estructurasdecision/3', methods=['POST'])
+def kinesticoestructurasdecision3():
+    try:
+        answer = "Tienes que pagar 5500€"
+        validation = False
+        if request.json['respuesta'].replace("  ","") == answer:
+            validation = True
+
+        return jsonify({
+            'validation': validation,
+            'answer': answer
+        }), 200
+    except Exception as e:
+        return f"an error ocurred : {e}"
+@ questionAPI.route('/kinestico/estructurasdecision/4', methods=['POST'])
+def kinesticoestructurasdecision4():
+    try:
+        answer = "Has cumplido 1 anos Has cumplido 2 anos Has cumplido 3 anos Has cumplido 4 anos Has cumplido 5 anos Has cumplido 6 anos Has cumplido 7 anos Has cumplido 8 anos"
+        validation = False
+        if request.json['respuesta'].replace("  ","") == answer:
+            validation = True
+
+        return jsonify({
+            'validation': validation,
+            'answer': answer
+        }), 200
+    except Exception as e:
+        return f"an error ocurred : {e}"
+
+@ questionAPI.route('/kinestico/estructurasdecision/5', methods=['POST'])
+def kinesticoestructurasdecision5():
+    try:
+        answer = "la multiplicacion de 1 x 1 es 1 la multiplicacion de 1 x 2 es 2 la multiplicacion de 1 x 3 es 3 la multiplicacion de 1 x 4 es 4 la multiplicacion de 1 x 5 es 5 la multiplicacion de 1 x 6 es 6 la multiplicacion de 1 x 7 es 7 la multiplicacion de 1 x 8 es 8 la multiplicacion de 1 x 9 es 9 la multiplicacion de 1 x 10 es 10 la multiplicacion de 2 x 1 es 2 la multiplicacion de 2 x 2 es 4 la multiplicacion de 2 x 3 es 6 la multiplicacion de 2 x 4 es 8 la multiplicacion de 2 x 5 es 10 la multiplicacion de 2 x 6 es 12 la multiplicacion de 2 x 7 es 14 la multiplicacion de 2 x 8 es 16 la multiplicacion de 2 x 9 es 18 la multiplicacion de 2 x 10 es 20 la multiplicacion de 3 x 1 es 3 la multiplicacion de 3 x 2 es 6 la multiplicacion de 3 x 3 es 9 la multiplicacion de 3 x 4 es 12 la multiplicacion de 3 x 5 es 15 la multiplicacion de 3 x 6 es 18 la multiplicacion de 3 x 7 es 21 la multiplicacion de 3 x 8 es 24 la multiplicacion de 3 x 9 es 27 la multiplicacion de 3 x 10 es 30 la multiplicacion de 4 x 1 es 4 la multiplicacion de 4 x 2 es 8 la multiplicacion de 4 x 3 es 12 la multiplicacion de 4 x 4 es 16 la multiplicacion de 4 x 5 es 20 la multiplicacion de 4 x 6 es 24 la multiplicacion de 4 x 7 es 28 la multiplicacion de 4 x 8 es 32 la multiplicacion de 4 x 9 es 36 la multiplicacion de 4 x 10 es 40 la multiplicacion de 5 x 1 es 5 la multiplicacion de 5 x 2 es 10 la multiplicacion de 5 x 3 es 15 la multiplicacion de 5 x 4 es 20 la multiplicacion de 5 x 5 es 25 la multiplicacion de 5 x 6 es 30 la multiplicacion de 5 x 7 es 35 la multiplicacion de 5 x 8 es 40 la multiplicacion de 5 x 9 es 45 la multiplicacion de 5 x 10 es 50 la multiplicacion de 6 x 1 es 6 la multiplicacion de 6 x 2 es 12 la multiplicacion de 6 x 3 es 18 la multiplicacion de 6 x 4 es 24 la multiplicacion de 6 x 5 es 30 la multiplicacion de 6 x 6 es 36 la multiplicacion de 6 x 7 es 42 la multiplicacion de 6 x 8 es 48 la multiplicacion de 6 x 9 es 54 la multiplicacion de 6 x 10 es 60 la multiplicacion de 7 x 1 es 7 la multiplicacion de 7 x 2 es 14 la multiplicacion de 7 x 3 es 21 la multiplicacion de 7 x 4 es 28 la multiplicacion de 7 x 5 es 35 la multiplicacion de 7 x 6 es 42 la multiplicacion de 7 x 7 es 49 la multiplicacion de 7 x 8 es 56 la multiplicacion de 7 x 9 es 63 la multiplicacion de 7 x 10 es 70 la multiplicacion de 8 x 1 es 8 la multiplicacion de 8 x 2 es 16 la multiplicacion de 8 x 3 es 24 la multiplicacion de 8 x 4 es 32 la multiplicacion de 8 x 5 es 40 la multiplicacion de 8 x 6 es 48 la multiplicacion de 8 x 7 es 56 la multiplicacion de 8 x 8 es 64 la multiplicacion de 8 x 9 es 72 la multiplicacion de 8 x 10 es 80 la multiplicacion de 9 x 1 es 9 la multiplicacion de 9 x 2 es 18 la multiplicacion de 9 x 3 es 27 la multiplicacion de 9 x 4 es 36 la multiplicacion de 9 x 5 es 45 la multiplicacion de 9 x 6 es 54 la multiplicacion de 9 x 7 es 63 la multiplicacion de 9 x 8 es 72 la multiplicacion de 9 x 9 es 81 la multiplicacion de 9 x 10 es 90 la multiplicacion de 10 x 1 es 10 la multiplicacion de 10 x 2 es 20 la multiplicacion de 10 x 3 es 30 la multiplicacion de 10 x 4 es 40 la multiplicacion de 10 x 5 es 50 la multiplicacion de 10 x 6 es 60 la multiplicacion de 10 x 7 es 70 la multiplicacion de 10 x 8 es 80 la multiplicacion de 10 x 9 es 90 la multiplicacion de 10 x 10 es 100 "
+        validation = False
+        if request.json['respuesta'].replace("  ","") == answer:
+            validation = True
+
+        return jsonify({
+            'validation': validation,
+            'answer': answer
+        }), 200
+    except Exception as e:
+        return f"an error ocurred : {e}"
+
+@ questionAPI.route('/kinestico/estructurasiterativa')
+def kinesticoestructurasiterativa():
+    try:
+        question = [
+            {
+                "question": " Escribir un programa dada una palabra, la muestre por pantalla 10 veces.",
+            },
+            {"question": "Escribir un programa que imprima los números del 1 al 10, pero que no imprima los numero 4 y 7.",
+
+             },
+            {
+                "question": "Escriba un programa que multiplique cada valor de una lista por si mismo, e imprima el total de esta. ",
+    
+            },
+            {
+                "question": "Escribir un programa que pregunte al usuario su edad y muestre por pantalla todos los años que ha cumplido (desde 1 hasta su edad).",
+            },
+            {
+                "question": "Escribir un programa que imprima las tablas de multiplicar del 1 al 10.",
+            }
+        ]
+        return jsonify(question), 200
+    except Exception as e:
+        return f"an error ocurred : {e}"
+
+@ questionAPI.route('/kinestico/estructurasiterativa/1', methods=['POST'])
+def kinesticoestructurasiterativa1():
+    try:
+        answer = "1 2 3 4 5 6 7 8 9 10"
+        validation = False
+        if request.json['respuesta'].replace("  ","") == answer:
+            validation = True
+
+        return jsonify({
+            'validation': validation,
+            'answer': answer
+        }), 200
+    except Exception as e:
+        return f"an error ocurred : {e}"
+
+@ questionAPI.route('/kinestico/estructurasiterativa/2', methods=['POST'])
+def kinesticoestructurasiterativa2():
+    try:
+        answer = "1 2 3 5 6 8 9 10"
+        validation = False
+        if request.json['respuesta'].replace("  ","") == answer:
+            validation = True
+
+        return jsonify({
+            'validation': validation,
+            'answer': answer
+        }), 200
+    except Exception as e:
+        return f"an error ocurred : {e}"
+
+@ questionAPI.route('/kinestico/estructurasiterativa/3', methods=['POST'])
+def kinesticoestructurasiterativa3():
+    try:
+        answer = "La multiplicacion de la lista da un total de 1680"
+        validation = False
+        if request.json['respuesta'].replace("  ","") == answer:
+            validation = True
+
+        return jsonify({
+            'validation': validation,
+            'answer': answer
+        }), 200
+    except Exception as e:
+        return f"an error ocurred : {e}"
+
+@ questionAPI.route('/kinestico/estructurasiterativa/4', methods=['POST'])
+def kinesticoestructurasiterativa4():
+    try:
+        answer = "El numero es impar"
+        validation = False
+        if request.json['respuesta'].replace("  ","") == answer:
+            validation = True
+
+        return jsonify({
+            'validation': validation,
+            'answer': answer
+        }), 200
+    except Exception as e:
+        return f"an error ocurred : {e}"
+@ questionAPI.route('/kinestico/funcionesiterativas')
+def kinesticofuncionesiterativas():
+    try:
+        question = [
+            {
+
+                "question":"Escribir una función que muestre por pantalla el saludo ¡Hola, amiga! cada vez que se la invoque.",
+            },
+            {"question": "Escribir una función a la que se le pase una cadena <nombre> y muestre por pantalla el saludo ¡hola <nombre>!.",
+
+             },
+            {
+                "question": "Escriba una función que reciba un argumento **kwargs y realice la suma de los values.",
+    
+            },
+            {
+                "question": "Escribir una función que calcule el máximo común divisor de dos números.",
+            }
+        ]
+        return jsonify(question), 200
     except Exception as e:
         return f"an error ocurred : {e}"
