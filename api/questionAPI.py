cc0887ba57a6da0b9a4de740f262e7b0afaa348d
Steven Otalvaro <stevenotalvaroz@gmail.com>
2022-10-31 16:11:05 -0500

    Carta de aprobacion

--- api/questionAPI.py
+++ api/questionAPI.py
@@ -3,7 +3,7 @@ from flask import Blueprint, request, jsonify
 questionAPI = Blueprint('questionAPI', __name__)
 
 
-@questionAPI.route('/ ')
+@questionAPI.route('/variables')
 def variables():
     try:
         question = [
